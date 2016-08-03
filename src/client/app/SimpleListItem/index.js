4c4c6a64d2a4fb85cd24023524462079b47bcd00
Xavier Lefèvre <xavier.lefevre@me.com>
2016-08-03 16:38:47 +0200

    Add Mentions code

---
+++ src/client/app/SimpleListItem/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react"
+import styles from "./index.css"
+import Feedback from "../Feedback"
+
+export default class SimpleListItem extends Component {
+
+  static propTypes = {
+    title: PropTypes.string,
+    onTouchTap: PropTypes.func.isRequired,
+    setAnyItemSelected: PropTypes.func.isRequired,
+    isAnyItemSelected: PropTypes.bool.isRequired,
+  };
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isSelected: false,
+    }
+  }
+
+  prepare() {
+    this._onSelect = this.props.onTouchTap && !this.props.isAnyItemSelected
+      ? (e) => {
+        e.preventDefault()
+        this.props.setAnyItemSelected(true)
+        setTimeout(this.props.onTouchTap, 150)
+      }
+      : () => {}
+
+    this.touchStart = () => {
+      this.onTouchTimeout = !this.props.isAnyItemSelected &&
+        setTimeout(() => {
+          this.setState({
+            isSelected: true,
+          })
+        }, 100)
+    }
+
+    this.touchMove = () => {
+      clearTimeout(this.onTouchTimeout)
+      this.setState({
+        isSelected: false,
+      })
+    }
+
+    this.touchEnd = () => {
+      setTimeout(() => {
+        this.setState({
+          isSelected: false,
+        })
+        this.props.setAnyItemSelected(false)
+      }, 500)
+      return true
+    }
+  }
+
+  render() {
+    this.prepare()
+
+    return (
+      <div
+        onTouchTap={ this._onSelect }
+        onTouchStart={ this.touchStart }
+        onTouchMove={ this.touchMove }
+        onTouchEnd={ this.touchEnd }
+        className={ styles.container }
+      >
+        { this.state.isSelected &&  <Feedback /> }
+        <div
+          className={ styles.item }
+        >
+          { this.props.title }
+        </div>
+      </div>
+
+    )
+  }
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
+import CommentInput from './CommentInput'
 
 class App extends React.Component {
   render () {
-    return <p>Yo Xavier!</p>;
+    <CommentInput />
   }
 }
 
